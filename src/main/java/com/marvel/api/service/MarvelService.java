d8385562824effbf80f9fc0c7df89151a8702c40
Apocalypse <aduzgun99@gmail.com>
2021-09-04 19:50:07 +0300

    hata!

--- src/main/java/com/marvel/api/service/MarvelService.java
+++ src/main/java/com/marvel/api/service/MarvelService.java
@@ -11,18 +11,18 @@ import com.marvel.api.client.MarvelCharactersClient;
 
 @Service
 public class MarvelService {
-    private static final String PUBLIC_KEY = "de8ae6b7ddc90c4bbcaba79435515c1a";
-    private static final String PRIVATE_KEY = "36411abdd8d7167a1f98760edadb5189be6b339d";
+    private static final String public_key = "";  ////////////  <-----
+    private static final String private_key = ""; ////////////  <-----
 
     private MarvelCharactersClient client;
 
     public Characters findAll() {
         Long timeStamp = new Date().getTime();
-        return client.getAll(timeStamp, PUBLIC_KEY, buildHash(timeStamp));
+        return client.getAll(timeStamp, public_key, buildHash(timeStamp));
     }
 
     private String buildHash(Long timeStamp) {
-        return DigestUtils.md5Hex(timeStamp + PRIVATE_KEY + PUBLIC_KEY);
+        return DigestUtils.md5Hex(timeStamp + private_key + public_key);
     }
 
     public MarvelService(MarvelCharactersClient client) {
